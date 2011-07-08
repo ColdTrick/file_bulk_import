cb2af226aac28fa41610863e4096c2984b5f63c4
rickterheide <rick@coldtrick.com>
2011-07-08 17:01:37 +0200

    Settings updated

--- views/default/settings/file_bulk_import/edit.php
+++ views/default/settings/file_bulk_import/edit.php
@@ -2,4 +2,12 @@
 	global $CONFIG;
 	
 	echo elgg_echo('file_bulk_import:settings:allowed_extensions');
-	echo elgg_view('input/text', array('internalname' => 'params[allowed_extensions]', 'value' => $vars['entity']->allowed_extensions)).'<br />';
+	if(!empty($vars['entity']->allowed_extensions))
+	{
+		$value = $vars['entity']->allowed_extensions;
+	}
+	else
+	{
+		$value = 'txt,jpg,jpeg,png,bmp,gif,pdf,doc,docx,xls,xlsx,pptx';
+	}
+	echo elgg_view('input/text', array('internalname' => 'params[allowed_extensions]', 'value' => $value)).'<br />';
